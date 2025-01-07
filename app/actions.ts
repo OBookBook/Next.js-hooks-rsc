77fdaa1afa78bf07e18e91b9311dd3df5d90da77
N.I <iworksWeb@gmail.com>
2025-01-07 20:33:33 +0900

    add useActionState()

--- app/actions.ts
+++ app/actions.ts
@@ -3,3 +3,10 @@
 export const formAction = async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
 };
+
+export const increment = async (previoutState: number) => {
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log(previoutState);
+
+  return previoutState + 1;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import { formAction } from "./actions";
+import { useActionState } from "react";
+import { formAction, increment } from "./actions";
 import IncrementBtn from "./IncrementBtn";
 
 export default function Home() {
+  const [state, formDispatch] = useActionState(increment, 0);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form action={formAction}>
+      <form action={formDispatch}>
         <IncrementBtn />
       </form>
+      <span>Count{state}</span>
     </main>
   );
 }
